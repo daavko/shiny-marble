dce2ae6736eeba2f5b20c2e6f68d5c0498b80139
daavko <mail@daavko.net>
2026-04-13 16:23:38 +0200

    small amount of more work

--- src/util/geometry.ts
+++ src/util/geometry.ts
@@ -13,11 +13,29 @@ export interface Dimensions {
     height: number;
 }
 
+export interface Rect extends Point, Dimensions {}
+
 export interface Extent {
     minX: number;
     minY: number;
     maxX: number;
     maxY: number;
+}
+
+export function intersectRects(rect1: Rect, rect2: Rect): Rect | null {
+    const minX = Math.max(rect1.x, rect2.x);
+    const minY = Math.max(rect1.y, rect2.y);
+    const maxX = Math.min(rect1.x + rect1.width, rect2.x + rect2.width);
+    const maxY = Math.min(rect1.y + rect1.height, rect2.y + rect2.height);
+
+    const intersectionWidth = maxX - minX;
+    const intersectionHeight = maxY - minY;
+
+    if (intersectionWidth <= 0 || intersectionHeight <= 0) {
+        return null;
+    } else {
+        return { x: minX, y: minY, width: intersectionWidth, height: intersectionHeight };
+    }
 }
 
 // todo: consider using a library like @flatten-js/core for geometry constructs (downside: can't be stored in IndexedDB)
